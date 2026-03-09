92806c9e7be50e8662df5f193f32eac522c5b300
BEUTSING <beutsingjeanne762@gmail.com>
2026-03-09 16:38:48 +0100

    Add a script to composer.json that tells the user to perform the migrations. Add the code snippet to allow Doctrine to identify our entity.

--- src/DependencyInjection/LogEntryExtension.php
+++ src/DependencyInjection/LogEntryExtension.php
@@ -9,10 +9,23 @@ class LogEntryExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $container->register(
+        $container->registerForAutoconfiguration(
             \Beutsing\LogEntryBundle\Service\LogEntryService::class)
-        ->setAutowired(true)
-        ->setAutoconfigured(true)
-        ->setPublic(true);
+                ->setAutowired(true)
+                ->setAutoconfigured(true)
+                ->setPublic(true);
+
+        $container->prependExtensionConfig('doctrine',[
+            "orm"=> [
+                'mappings'=> [
+                    'is_bundle'=> false,
+                    'type'=>'attribute',
+                    'dir'=> '%kernel.project_dir%/vendor/beutsing/log-entry-bundle/src/Entity',
+                    'prefix'=> 'Beutsing\LogEntryBundle\Entity',
+                    'alias'=> 'LogEntryBundle'
+            
+                ],
+            ],
+        ]);        
     }
 }
